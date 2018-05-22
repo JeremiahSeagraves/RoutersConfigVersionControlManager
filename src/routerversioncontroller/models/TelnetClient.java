a5510b7ace2716c6727d33bb180ec1a8acdc1dd7
Noe Cutz <noecutz03@gmail.com>
2018-05-22 18:00:38 -0500

    Indentación en telnetClient

--- src/routerversioncontroller/models/TelnetClient.java
+++ src/routerversioncontroller/models/TelnetClient.java
@@ -15,61 +15,61 @@ import java.util.List;
 import java.util.ArrayList;
 import java.net.*;
 
+public class TelnetClient {
 
-public class TelnetClient{
+    public void connectTelnet(String ip) {
 
-	public void connectTelnet(String ip){
+        try {
+            Socket soc = new Socket(ip, 23);
+            //create buffered writer
+            BufferedReader bwin = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+            while (true) {
+                String readFir = bwin.readLine();
+                if (readFir == null) {
+                    break;
+                }
+                System.out.println(readFir);
+                if (readFir.startsWith("Password")) {
 
-		try{
-			Socket soc = new Socket(ip,23);
-			//create buffered writer
-			BufferedReader bwin = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
-			while (true) {
-				String readFir = bwin.readLine();
-				if (readFir == null) break;
-				System.out.println(readFir);
-				if(readFir.startsWith("Password")){
+                    //Escribimos la contrasenia
+                    bw.write("cisco");
+                    bw.newLine();
+                    bw.flush();
 
-			        	//Escribimos la contrasenia
-					bw.write("cisco");
-					bw.newLine();
-					bw.flush();
+                    //Entramos modo privilegiado
+                    bw.write("enable");
+                    bw.newLine();
+                    bw.flush();
 
-			            //Entramos modo privilegiado
-					bw.write("enable");
-					bw.newLine();
-					bw.flush();
+                    //Escribimos la contrasenia del modo privilegiado
+                    bw.write("class");
+                    bw.newLine();
+                    bw.flush();
 
-			            //Escribimos la contrasenia del modo privilegiado
-					bw.write("class");
-					bw.newLine();
-					bw.flush();
+                    //Mostramos el archivo de configuracion
+                    bw.write("show startup-config");
+                    bw.newLine();
 
-			            //Mostramos el archivo de configuracion
-					bw.write("show startup-config");
-					bw.newLine();
-					
-					bw.flush();
+                    bw.flush();
 
-					String configuration = bwin.readLine();
+                    String configuration = bwin.readLine();
 
-					System.out.println(configuration);
+                    System.out.println(configuration);
 
-			            // instead might have to explicitly write "\r\n"
-			            // depending platform you're connecting from.
-					bw.flush();
-				}
+                    // instead might have to explicitly write "\r\n"
+                    // depending platform you're connecting from.
+                    bw.flush();
+                }
 
-			}
-		}catch(ConnectException e){
-			System.out.println("Tiempo de conexion excedido");
-		}catch(UnknownHostException e){
-			System.out.println("Servidor no encontrado");
-		}catch(IOException e){
-			System.out.println("Error de lectura");
-		}
-		
-		
-	}
+            }
+        } catch (ConnectException e) {
+            System.out.println("Tiempo de conexion excedido");
+        } catch (UnknownHostException e) {
+            System.out.println("Servidor no encontrado");
+        } catch (IOException e) {
+            System.out.println("Error de lectura");
+        }
+
+    }
 }
