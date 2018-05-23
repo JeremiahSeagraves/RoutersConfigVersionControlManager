4db1a609d0e2b4702141a4b52bb13e548bdfa1a9
Noe Cutz <noecutz03@gmail.com>
2018-05-22 23:25:34 -0500

    Implementado el guardado de archivos por versionado

--- src/routerversioncontroller/models/VersionController.java
+++ src/routerversioncontroller/models/VersionController.java
@@ -7,65 +7,94 @@ package routerversioncontroller.models;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
 
 /**
  *
  * @author NoeCutz
  */
 public class VersionController {
-    
-    public void getConfigurationFiles(){
-        
-        
-        
-        TelnetClient telnetClient = new TelnetClient();
-        
-        
-        IpFinder ipFinder =  new IpFinder();
+
+    private FileManager fileManager;
+    private TelnetClient telnetClient;
+
+    public VersionController() {
+        fileManager = new FileManager();
+        telnetClient = new TelnetClient();
+    }
+
+    public void createFiles(ArrayList<String> devices) {
+
+        fileManager.createFolders();
+
+        IpFinder ipFinder = new IpFinder();
         String ip = ipFinder.getIpAddress();
-        
-        
-        telnetClient.connectTelnet("");  
-        
-        String configurationFile  = telnetClient.getConfigurationFile();
-        
-        saveConfigurationFile("", configurationFile);
-        
-        
+
+        for (int i = 0; i < devices.size(); i++) {
+
+            telnetClient.connectTelnet(devices.get(i));
+
+            String configurationFile = telnetClient.getConfigurationFile();
+            saveConfigurationFile(devices.get(i), configurationFile);
+        }
+
+    }
+
+    public void saveConfigurationFile(String folder, String info) {
+
+        File directory = new File("configurationFiles/" + folder);
+        File[] files = directory.listFiles();
+
+        int numFiles = files.length;
+
+        if (numFiles == 0) {
+            createCurrentConfigurationFile(folder, info);
+        } else if (numFiles > 0) {
+
+            //Se busca el ultimo archivo de configuración guardado
+            File lastFile = getlastConfigurationFile(files);
+            
+
+            //Al ultimo archivo se le cambia el nombre a uno con versionado
+            String newName = folder + "/" + folder + "_v" + numFiles;
+            File newFile = new File(newName);
+            lastFile.renameTo(lastFile);
+
+            //Se crea el nuevo archivo
+            createCurrentConfigurationFile(folder, info);
+
+        }
+
     }
     
-    
-    public void saveConfigurationFile(String folder, String info){
-        FileManager fileManager =  new FileManager();
-        fileManager.createFolders();
-        
-        File directory = new File("configurationFiles/");
-        File[] files = directory.listFiles();
-        
-        int numFiles = files.length;
-        
-        
-        if(numFiles == 0){
-           String nameFile = "Actual";
-           fileManager.writeFile(nameFile, info);
-        }else if(numFiles > 0){
+    private void updateConfigurationFiles(ArrayList<String> devices){
+         for (int i = 0; i < devices.size(); i++) {
+            telnetClient.connectTelnet(devices.get(i));
+            String configurationFile = telnetClient.getConfigurationFile();
             
-            File lastFile = null ;
-            for(int j=0; j< numFiles;j++){
-                if(files[j].getName().contains("Actual")){
-                    lastFile = files[j];
-                }
+             //getlastConfigurationFile(files)
+            
+            saveConfigurationFile(devices.get(i), configurationFile);
+        }
+    }
+
+    private void createCurrentConfigurationFile(String folder, String text) {
+        String nameFile = folder + "/" + folder + "_Actual";
+        fileManager.writeFile(nameFile, text);
+    }
+
+    private File getlastConfigurationFile(File[] files) {
+
+        File lastFile= null;
+        
+        for (int j = 0; j < files.length; j++) {
+            if (files[j].getName().contains("Actual")) {
+                 lastFile = files[j];
+                 return lastFile;
             }
-            
-            String newName = "";
-            
-            File newFile = new File(newName);
-            
-            lastFile.renameTo(lastFile);
-               
         }
         
+        return lastFile;
     }
-    
-    
+
 }
